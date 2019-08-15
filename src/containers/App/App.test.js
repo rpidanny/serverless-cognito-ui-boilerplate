f2b635fc6b06594c59e69e76e4f494a1a20e5b59
rpidanny <abhishekmaharjan1993@gmail.com>
2019-08-15 14:48:23 +0200

    Update test script

--- src/containers/App/App.test.js
+++ src/containers/App/App.test.js
@@ -1,10 +1,17 @@
 /* global it */
 import React from 'react'
 import ReactDOM from 'react-dom'
-import App from './App'
+import { BrowserRouter as Router } from 'react-router-dom'
+
+import App from './index'
 
 it('renders without crashing', () => {
   const div = document.createElement('div')
-  ReactDOM.render(<App />, div)
+  ReactDOM.render(
+    <Router
+      basename={process.env.PUBLIC_URL + '/'}
+    >
+      <App />
+    </Router>, div)
   ReactDOM.unmountComponentAtNode(div)
 })
