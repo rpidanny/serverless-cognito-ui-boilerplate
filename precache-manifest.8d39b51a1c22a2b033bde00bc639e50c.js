ba59717ecffb7aca010e26975261c077b769f8e3
rpidanny (from Travis CI) <abhishekmaharjan1993@gmail.com>
2019-08-30 17:06:26 +0000

    Deploy rpidanny/serverless-cognito-ui-boilerplate to github.com/rpidanny/serverless-cognito-ui-boilerplate.git:gh-pages

---
+++ precache-manifest.8d39b51a1c22a2b033bde00bc639e50c.js
@@ -0,0 +1,54 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "35f5e1b1726f8a388f2a9d3372b8d1a2",
+    "url": "/index.html"
+  },
+  {
+    "revision": "a079dbdf582ca57ad31d",
+    "url": "/static/css/2.eee0f4a3.chunk.css"
+  },
+  {
+    "revision": "d23e2c84e0177236b943",
+    "url": "/static/css/4.cf7371b3.chunk.css"
+  },
+  {
+    "revision": "8c4b97bb9bba9e645aea",
+    "url": "/static/css/5.91dc095c.chunk.css"
+  },
+  {
+    "revision": "4916e5e6465b98713569",
+    "url": "/static/css/main.23c5b774.chunk.css"
+  },
+  {
+    "revision": "a079dbdf582ca57ad31d",
+    "url": "/static/js/2.b36c3dc2.chunk.js"
+  },
+  {
+    "revision": "4b23d60699c0d6022f87",
+    "url": "/static/js/3.7f80a9e0.chunk.js"
+  },
+  {
+    "revision": "d23e2c84e0177236b943",
+    "url": "/static/js/4.c3d5610f.chunk.js"
+  },
+  {
+    "revision": "8c4b97bb9bba9e645aea",
+    "url": "/static/js/5.13e6072e.chunk.js"
+  },
+  {
+    "revision": "4916e5e6465b98713569",
+    "url": "/static/js/main.72db27d3.chunk.js"
+  },
+  {
+    "revision": "393e774bf74d1565bf71",
+    "url": "/static/js/runtime~main.8cede295.js"
+  },
+  {
+    "revision": "5d5d9eefa31e5e13a6610d9fa7a283bb",
+    "url": "/static/media/logo.5d5d9eef.svg"
+  },
+  {
+    "revision": "ba2f093229c34e0b3b50bd8542a20e59",
+    "url": "/static/media/serverless.ba2f0932.png"
+  }
+]);
