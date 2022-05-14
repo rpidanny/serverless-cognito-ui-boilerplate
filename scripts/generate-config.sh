4722d66f2e2874ba8ab3405f004770594d6032f2
rpidanny <abhishekmaharjan1993@gmail.com>
2022-05-14 12:35:41 +0545

    fix: generate config script

--- scripts/generate-config.sh
+++ scripts/generate-config.sh
@@ -1,3 +1,3 @@
 echo "Generating Config from Template"
 
-sed "s/YOUR_API_GATEWAY_ID/$API_GATEWAY_ID/g;s/YOUR_S3_BUCKET_NAME/$S3_BUCKET_NAME/g;s/YOUR_REGION/$REGION/g;s/YOUR_USER_POOL_ID/$USER_POOL_ID/g;s/YOUR_APP_CLIENT_ID/$APP_CLIENT_ID/g;s/YOUR_IDENTITY_POOL_ID/$IDENTITY_POOL_ID/g" ./src/config/amplify/amplify.config.example.ts > ./src/config/amplify/amplify.config.ts
+sed "s@YOUR_API_GATEWAY_ID@$API_GATEWAY_ID@g;s@YOUR_S3_BUCKET_NAME@$S3_BUCKET_NAME@g;s@YOUR_REGION@$REGION@g;s@YOUR_USER_POOL_ID@$USER_POOL_ID@g;s@YOUR_APP_CLIENT_ID@$APP_CLIENT_ID@g;s@YOUR_IDENTITY_POOL_ID@$IDENTITY_POOL_ID@g" ./src/config/amplify/amplify.config.example.ts > ./src/config/amplify/amplify.config.ts
